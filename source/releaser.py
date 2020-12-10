c6b99962e8871a716adcce365fce8ff0d7774e5f
Ivan Zlatanov <ivanff1@abv.bg>
2020-12-10 17:07:48 +0200

    Updates in releaser.py

--- source/releaser.py
+++ source/releaser.py
@@ -90,7 +90,7 @@ class Releaser():
 
         print('>>> Building release archive.')
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        build_tar_command = 'tar -czf {}/release/core-{}.tar.gz {}/../repo'.format(dir_path, version, dir_path)
+        build_tar_command = 'tar -czf {}/release/core-{}.tar.gz {}/../../repo'.format(dir_path, version, dir_path)
 
         try:
             subprocess.check_call(build_tar_command.split())
